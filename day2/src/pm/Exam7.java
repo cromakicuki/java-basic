206d4fa62a63a950dc5d59574715630ed0c2b356
cromakicuki <98381613+cromakicuki@users.noreply.github.com>
2022-03-03 16:49:41 +0900

    update

--- day2/src/pm/Exam7.java
+++ day2/src/pm/Exam7.java
@@ -4,14 +4,6 @@ public class Exam7 {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
-//		int sum = 0;
-//		for (int i = 0; i <= 1000; i++) {
-//			if (i % 3 == 0) {
-//				sum = sum + i;
-//			}
-//		}
-//		System.out.println(sum);
 		
 		String j = "*";
 		for (int i = 1; i <= 5; i++) {
